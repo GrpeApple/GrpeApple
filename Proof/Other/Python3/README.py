529f403546834ef5254dc5218a1ac54ac0e1af76
Grapple <grapeapplemelonwater@gmail.com>
2021-05-10 19:58:56 +0800

    Read from file

--- Proof/Other/Python3/README.py
+++ Proof/Other/Python3/README.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 
-print('Once a programmer, always a programmer.')
+from os.path import dirname, abspath
+
+with open(dirname(abspath(__file__)) + '/README.md', mode='rt', buffering=1, closefd=True) as file:
+    print(file.read(), end='', flush=True)
